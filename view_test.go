737955e393f3db29319e890584aaa0749c4b818e
Yoan Blanc <yoan@dosimple.ch>
2017-07-03 12:49:51 +0200

    Add test for JSON-LD (and refactoring)
    Signed-off-by: Yoan Blanc <yoan@dosimple.ch>

---
+++ view_test.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestInfoAsJson(t *testing.T) {
+	r := makeRouter()
+	ts := httptest.NewServer(r)
+	defer ts.Close()
+
+	url := ts.URL + "/test.png/info.json"
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if status := resp.StatusCode; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+
+	if contentType := resp.Header.Get("Content-Type"); contentType != "application/json" {
+		t.Errorf("handle should return JSON by default: got %v want application/json", contentType)
+	}
+}
+
+func TestInfoAsJsonLd(t *testing.T) {
+	r := makeRouter()
+	ts := httptest.NewServer(r)
+	defer ts.Close()
+
+	req, err := http.NewRequest("GET", ts.URL+"/test.png/info.json", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Add("Accept", "application/ld+json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if status := resp.StatusCode; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+
+	if contentType := resp.Header.Get("Content-Type"); contentType != "application/ld+json" {
+		t.Errorf("handle should return JSON by default: got %v want application/ld+json", contentType)
+	}
+}
